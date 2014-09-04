fc52e22b1a95fb94a6df0f073abfbf4433b0e827
Tim Schindler <tim@catalyst-zero.com>
2014-09-04 13:25:13 +0000

    added Exist() call

---
+++ exist.go
@@ -0,0 +1,22 @@
+package client
+
+import (
+	apiSchemaPkg "github.com/catalyst-zero/api-schema"
+)
+
+func (this *Client) Exist(userId string) (bool, error) {
+	zeroVal := false
+
+	res, err := this.get(this.endpointUrl("/token/" + userId + "/exist"))
+	if err != nil {
+		return zeroVal, Mask(err)
+	}
+
+	// Check token service response.
+	var tokenExist bool
+	if err := apiSchemaPkg.ParseData(&res.Body, &tokenExist); err != nil {
+		return zeroVal, Mask(err)
+	}
+
+	return tokenExist, nil
+}
