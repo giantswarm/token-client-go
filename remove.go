7739874e307e3aa466b64e678b80e63fecdd09fa
Ewout Prangsma <ewout@prangsma.net>
2015-02-10 07:58:06 +0000

    Added RemoveAll

--- remove.go
+++ remove.go
@@ -12,3 +12,13 @@ func (this *Client) Remove(token string) (*http.Response, error) {
 
 	return res, nil
 }
+
+// RemoveAll will remove all tokens for the given user id
+func (this *Client) RemoveAll(userId string) (*http.Response, error) {
+	res, err := this.delete(this.endpointUrl("/user/" + userId))
+	if err != nil {
+		return nil, Mask(err)
+	}
+
+	return res, nil
+}
